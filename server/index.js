a5bf36ce8d5f748a89ea421f7b40fbb5f9f4dd93
Harsh <harshdtu2002@gmail.com>
2025-12-25 14:59:15 +0530

    connect MongoDB Atlas to backend

--- server/index.js
+++ server/index.js
@@ -4,9 +4,12 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("API running");
